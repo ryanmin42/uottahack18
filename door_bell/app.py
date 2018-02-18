444e870bebc53abc10b1b3beb0321ff03ed620c2
Korhan Akcura <korhan.akcura@openbet.com>
2018-02-17 21:44:29 -0500

    Door Bell - Initial version. Take picture and post it to a service.

---
+++ door_bell/app.py
@@ -0,0 +1,33 @@
+# Door Bell Service : A Web Service to run from a smart door bell with a camera.
+# Author :  Korhan Akcura
+
+import json
+import requests
+import requests
+import cv2
+import base64
+
+from flask import Flask, jsonify
+
+app = Flask(__name__, static_url_path='')
+
+@app.route("/")
+def root():
+	return app.send_static_file('index.html')
+
+def take_and_send_picture():
+	# initialize the camera
+	cam = cv2.VideoCapture(0)
+	ret, image = cam.read()
+	if ret:
+		cv2.imwrite('./static/images/send_image.jpg',image)
+		retval, buffer = cv2.imencode('.jpg', image)
+		jpg_as_text = base64.b64encode(buffer)
+	cam.release()
+	send_picture(jpg_as_text)
+
+def send_picture(image):
+	imageJSON = json.dumps({'picture' : image.encode('base64')})
+	res = requests.post('http://localhost:3000/api/image', json=imageJSON)
+
+take_and_send_picture()
