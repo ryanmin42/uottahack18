bd2a490a01d39bf6c3add015d5cffee9d07b3e78
Ryan Min <ryanmin42@gmail.com>
2018-02-18 04:53:15 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,32 +2,10 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var Particle = require('particle-api-js');
 var bodyParser = require('body-parser');
-var particle = new Particle();
-var server = express();
-var token = '5f90f7cd63385fc06c946b1779b650198cec72ee';
+var TextToSpeech = require('text-to-speech-js');
 var picture;
 var statement;
-
-particle.login({username: 'muizchunara98@gmail.com', password: '98ztr4jg'}).then(
-function(data) {
-  token = data.body.access_token;
-},
-function (err) {
-  console.log('Could not log in.', err);
-}
-);
-var devicesPr = particle.listDevices({ auth: token });
-
-devicesPr.then(
-function(devices){
-  console.log('Devices: ', devices);
-},
-function(err) {
-  console.log('List devices call failed: ', err);
-}
-);
 
 server.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -48,11 +26,8 @@ server.get('/api/html', function(req, res) {
 });
 
 server.get('/api/results', function(req, res) {
-  statement = req.body.results;
-});
-
-server.post('/api/particle', function(req, res) {
-  res.json({"results": statement});
+  statement = req.body.result;
+  TextToSpeech.talk(statement);
 });
 
 server.listen(3000, function () {
